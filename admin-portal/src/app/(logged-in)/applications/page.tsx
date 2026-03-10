0d24ddb8a1797a41955ac2f807448f0e4d4a24aa
anaspolygon <al.anas@polygontech.xyz>
2026-03-10 11:27:53 +0600

    update

--- admin-portal/src/app/(logged-in)/applications/page.tsx
+++ admin-portal/src/app/(logged-in)/applications/page.tsx
@@ -1,13 +1,9 @@
 import { ApplicationSection } from "@polygontech/application-details";
 
-
 const page = () => {
   return (
     <div className="flex flex-col gap-8">
-      <ApplicationSection
-        url={`${process.env.NEXT_API_ADMIN_BASE_URL}/api/admin/applications`}
-        apiKey={process.env.NEXT_API_KEY as string}
-      />
+      <ApplicationSection apiKey={process.env.NEXT_API_KEY as string} />
     </div>
   );
 };
