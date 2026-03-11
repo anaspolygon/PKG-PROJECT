5fc888b1f101fa8329620c08823e8f11cfc307b7
anaspolygon <al.anas@polygontech.xyz>
2026-03-11 12:38:35 +0600

    update

--- admin-portal/src/app/(logged-in)/applications/[id]/page.tsx
+++ admin-portal/src/app/(logged-in)/applications/[id]/page.tsx
@@ -1,14 +1,11 @@
-import { ApplicationDetailsWapper } from "@polygontech/application-details";
+import { ApplicationDetails } from "@polygontech/application-details";
 import React from "react";
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
-  const url = `${process.env.NEXT_API_ADMIN_BASE_URL}/api/application/${id}`;
-  const preloadUrl = `${process.env.NEXT_API_ADMIN_BASE_URL}/api/preload-data`;
   return (
-    <ApplicationDetailsWapper
-      // url={url}
-      // preloadUrl={preloadUrl}
+    <ApplicationDetails
+      baseUrl={process.env.NEXT_API_ADMIN_BASE_URL as string}
       id={id}
       showActionsBtn
       apiKey={process.env.NEXT_API_KEY as string}
